7ab71ae24e4432e99bf55f578535735ad497149b
Elham Fabe <elhamfabe@gmail.com>
2020-07-30 18:05:03 -0400

    create .js file and comments

---
+++ Jscript.js
@@ -0,0 +1,25 @@
+// create variables to store/loop
+
+
+
+// moment.js data for date (header)
+
+
+
+
+// create scheduling blocks
+
+
+
+// create save button
+
+
+
+// local storage 
+
+
+
+// save data to local storage
+
+
+// view exsisting local storage 
