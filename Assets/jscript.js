7f8e2f0df3b401d88fc5032d1661761dc80f9f35
Elham Fabe <elhamfabe@gmail.com>
2020-07-31 00:44:04 -0400

    rows

--- Assets/jscript.js
+++ Assets/jscript.js
@@ -1,9 +1,62 @@
 // create variables to store/loop
 let daySch = [
     {
-        id: "",
-        hour: "",
-    }
+        id: "0",
+        hour: "06",
+    },
+    {
+        id: "1",
+        hour: "07",
+    },
+    {
+        id: "2",
+        hour: "08",
+    },
+    {
+        id: "3",
+        hour: "09",
+    },
+    {
+        id: "4",
+        hour: "10",
+    },
+    {
+        id: "5",
+        hour: "11",
+    },
+    {
+        id: "6",
+        hour: "12",
+    },
+    {
+        id: "7",
+        hour: "01",
+    },
+    {
+        id: "8",
+        hour: "02",
+    },
+    {
+        id: "9",
+        hour: "03",
+    },
+    {
+        id: "10",
+        hour: "04",
+    },
+    {
+        id: "11",
+        hour: "05",
+    },
+    {
+        id: "12",
+        hour: "06",
+    },
+    {
+        id: "13",
+        hour: "07",
+    },
+    
 
 ]
 
@@ -18,8 +71,17 @@ function momentDate() {
 
 momentDate();
 
-// create scheduling blocks
-let timeBlock = $("<div class='daysch'>");
+// create scheduling form / rows
+daySch.forEach(function (timeBlock) {
+    let timeRow = $("<form>").attr({
+        "class": "row"
+    });
+$(".container").append(timeRow);
+console.log(timeRow);
+
+// create time for rows
+let hourRow = $("<div>").text()
+});
 
 
 // create save button
@@ -27,7 +89,7 @@ let saveBtn = $("<i class='fas fa-bolt'></i>");
 let saveSch = $("<button>").attr("class", "col-md-1")
 
 
-
+});
 // local storage 
 
 
