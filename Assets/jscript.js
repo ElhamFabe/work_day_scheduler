3beb2d0721f49f0feedb912fa8eb3cfca3b8d5c5
Elham Fabe <elhamfabe@gmail.com>
2020-08-01 22:53:04 -0400

    agenda added to variables

--- Assets/jscript.js
+++ Assets/jscript.js
@@ -5,6 +5,7 @@ let daySch = [
         hour: "08",
         time:"08",
         mid:"am",
+        agenda: ""
 
     },
     {
@@ -12,72 +13,84 @@ let daySch = [
         hour: "09",
         time:"09",
         mid:"am",
+        agenda: ""
     },
     {
         id: "2",
         hour: "10",
         time: "10",
         mid:"am",
+        agenda: ""
     },
     {
         id: "3",
         hour: "11",
         time: "11",
         mid:"am",
+        agenda: ""
     },
     {
         id: "4",
         hour: "12",
         time: "12",
         mid:"pm",
+        agenda: ""
     },
     {
         id: "5",
         hour: "01",
         time: "01",
         mid:"pm",
+        agenda: ""
     },
     {
         id: "6",
         hour: "02",
-        time:
-        mid:"pm",
+        time:"02",
+        mid: "pm",
+        agenda: ""
     },
     {
         id: "7",
         hour: "03",
-        time:
+        time: "03",
         mid:"pm",
+        agenda: ""
     },
     {
         id: "8",
         hour: "04",
-        time:
+        time: "04",
         mid:"pm",
+        agenda: ""
     },
     {
         id: "9",
         hour: "05",
-        time:
-        mid:"pm",
+        time: "05",
+        mid: "pm",
+        agenda:""
     },
     {
         id: "10",
         hour: "06",
-        time:
-        mid:"pm",
+        time: "06",
+        mid: "pm",
+        agenda:""
     },
     {
         id: "11",
-        hour: "08",
-        time:
-        mid:"pm",
+        hour: "07",
+        time: "07",
+        mid: "pm",
+        agenda:""
     },
     {
         id: "12",
         hour: "08",
-        time:
-        mid:"pm",
+        time: "08",
+        mid: "pm",
+        agenda:""
     }
 ]
 
